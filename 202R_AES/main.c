e141cf7f3fea6aa2f01d43d14bf458f2608d9ace
sqrtrev <sqrtrev@korea.ac.kr>
2020-03-18 13:36:57 +0900

    working on main.c

--- 202R_AES/main.c
+++ 202R_AES/main.c
@@ -2,19 +2,40 @@
 #include <stdio.h>
 #define BLOCK_SIZE 16
 
+volatile char* plaintext;
+volatile char* result;
+void readfile(const char*);
+
 int main(int argc, char *argv[]) {
 	const char* key;
-	char plaintext[129];
+	
 
-	if (argc != 3) {
-		printf("Usage: ./aes file_dir key");
-		return -1;
-	}
-	if (sizeof(key) != 16 && sizeof(key) != 24 && sizeof(key) != 32) {
-		printf("AES Key length error");
-		return -1;
-	}
+	if (argc != 3) 
+		perror("Usage: ./aes filename key");
+	
+	if (sizeof(key) != 16 && sizeof(key) != 24 && sizeof(key) != 32) 
+		perror("AES Key length error");
+	
 	key = argv[2];
 
-	FILE *f = fopen(argv[1], "rb");
+	readfile(argv[1], plaintext);
+
+
+	free(plaintext);
+	return 0;
+}
+
+void readfile(const char* filename) {
+	FILE *fp = fopen(filename, "rb");
+	unsigned long size;
+
+	if (fp == NULL)
+		perror("Error during opening file");
+
+	fseek(fp, 0, SEEK_END);
+	size = fteel(fp);
+	fseek(fp, 0, SEEK_SET);
+	
+	plaintext = (char *)malloc(fp + 1);
+	fgets(plaintext, size, fp);
 }
